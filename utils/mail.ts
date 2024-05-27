e8e11e3c9ef4fa158d319637165626754d8c4cd9
shadinmhd <shadinmhd97@gmail.com>
2024-05-27 13:26:03 +0530

    update: changed email for otp to magic link

--- utils/mail.ts
+++ utils/mail.ts
@@ -13,26 +13,12 @@ const transporter = nodemailer.createTransport({
 })
 
 
-export const sendOtp = (to: string, code: string) => {
+export const sendOtp = (to: string, url: string) => {
 	const mailOptions: MailOptions = {
 		from: GMAIL,
 		to: to,
 		subject: "Otp for login in reps",
-		html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Reps</a>
-    </div>
-    <p style="font-size:1.1em">Hi,</p>
-    <p>Thank you for choosing Reps. Use the following OTP to complete your Registration procedures. OTP is valid for 5 minutes</p>
-    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${code}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Reps</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>Reps</p>
-    </div>
-  </div>
-</div>`
+		html: sendOtpEmail(url)
 	}
 
 	transporter.sendMail(mailOptions, (error, info) => {
@@ -44,3 +30,60 @@ export const sendOtp = (to: string, code: string) => {
 	})
 }
 
+
+const sendOtpEmail = (url: string) => {
+	return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Please activate your account</title>
+  <!--[if mso]><style type="text/css">body, table, td, a { font-family: Arial, Helvetica, sans-serif !important; }</style><![endif]-->
+</head>
+
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+  <table role="presentation" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom=""
+    data-darkreader-inline-border-left=""
+    style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px; background-color: rgb(239, 239, 239); --darkreader-inline-bgcolor: #212425;"
+    data-darkreader-inline-bgcolor="">
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+          <table role="presentation" data-darkreader-inline-border-top="" data-darkreader-inline-border-right=""
+            data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left=""
+            style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;">
+            <tbody>
+              <tr>
+                <td style="padding: 40px 0px 0px;">
+                  <div style="text-align: center;">
+                    <!---->
+                  </div>
+                  <div style="padding: 20px; background-color: rgb(255, 255, 255); --darkreader-inline-bgcolor: #181a1b;"
+                    data-darkreader-inline-bgcolor="">
+                    <div style="color: rgb(0, 0, 0); text-align: left; --darkreader-inline-color: #e8e6e3;" data-darkreader-inline-color="">
+                      <h1 style="margin: 1rem 0">Final step...</h1>
+                      <p style="padding-bottom: 16px">Follow this link to verify your email address.</p>
+                      <p style="padding-bottom: 16px"><a href="${url}" target="_blank"
+                          style="padding: 12px 24px; border-radius: 4px; color: rgb(255, 255, 255); background: rgb(43, 82, 245); display: inline-block; margin: 0.5rem 0px; --darkreader-inline-color: #e8e6e3; --darkreader-inline-bgimage: initial; --darkreader-inline-bgcolor: #0829b0;"
+                          data-darkreader-inline-color="" data-darkreader-inline-bgimage="" data-darkreader-inline-bgcolor="">Confirm now</a></p>
+                      <p style="padding-bottom: 16px">If you didn’t ask to verify this address, you can ignore this email.</p>
+                      <p style="padding-bottom: 16px">Thanks,<br>The Reps</p>
+                    </div>
+                  </div>
+                  <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center; --darkreader-inline-color: #a8a095;"
+                    data-darkreader-inline-color="">
+                    <p style="padding-bottom: 16px">Made with ♥ in India</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>`
+}
