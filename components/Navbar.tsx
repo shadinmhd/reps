ca9a9d5e869e16da5e88df4f79cb6abf19eb8880
shadinmhd <shadinmhd97@gmail.com>
2024-05-29 12:19:52 +0530

    update: added drop down menu in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,11 @@
 import Flexing from '@/assets/icons/Flexing'
 import { useUser } from '@/context/userContext'
 import Link from 'next/link'
-import React from 'react'
+import { ReactNode } from 'react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from './shadcn/DropDownMenu'
+import Exit from '@/assets/icons/Exit'
+import Gear from '@/assets/icons/Gear'
+import DashboardIcon from '@/assets/icons/DashboardIcon'
 
 const Navbar = () => {
 
@@ -10,17 +14,18 @@ const Navbar = () => {
 
 	return (
 		<header className='flex items-center justify-between p-5 px-10 w-full'>
-			<Link href={"/"} className='flex gap-2 items-center font-bold text-2xl'>
+			<Link href={"/"} className='flex gap-2 items-center font-bold text-3xl'>
 				<Flexing className='text-5xl' />
 				Reps
 			</Link>
 			{
 				loggedIn ?
-					<p className='flex items-center text-xl font-bold'>
+					<Menu className='flex items-center text-2xl font-bold' >
 						{user?.username}
-					</p> :
+					</Menu>
+					:
 					<div className='flex items-center gap-2'>
-						<Link href={"/login"} className='font-semibold p-2 rounded-md bg-custom-gray'>
+						<Link href={"/login"} className='font-semibold p-2 rounded-md bg-custom-gray outline-none'>
 							Login
 						</Link>
 						<Link href={"/register"} className='bg-white text-black font-semibold p-2 rounded-md'>
@@ -29,6 +34,39 @@ const Navbar = () => {
 					</div>
 			}
 		</header>
+	)
+}
+
+const Menu = ({ children, className }: { children: ReactNode, className: string }) => {
+
+	const { logout } = useUser()
+
+	return (
+		<DropdownMenu >
+			<DropdownMenuTrigger className={className}>
+				{children}
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<Link href={"/dash"} className='flex items-center gap-2'>
+							<DashboardIcon className='text-xl' />
+							<p className='font-bold text-lg'>Dashboard</p>
+						</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Link href={"/dash/settings"} className='flex items-center gap-2'>
+							<Gear className='text-xl' />
+							<p className='font-bold text-lg'>Settings</p>
+						</Link>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuItem onClick={logout} className='flex gap-2 items-center cursor-pointer'>
+					<Exit className='text-xl' />
+					<p className='font-bold text-lg'>Logout</p>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	)
 }
 
