2f2a0bcb79f36e2613af5cab04b182dcf4e01257
shadinmhd <shadinmhd97@gmail.com>
2024-05-29 12:22:46 +0530

    fix: missing files in assets folder

---
+++ assets/icons/DashboardIcon.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from 'react';
+
+const DashboardIcon = (props: SVGProps<SVGSVGElement>) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 24 24"
+			{...props}>
+			<path fill="white" d="M3 12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1zm0 8a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1zm10 0a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-8a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1zm1-17a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1z"></path>
+		</svg>)
+}
+
+export default DashboardIcon
---
+++ assets/icons/Exit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+const Exit = (props: SVGProps<SVGSVGElement>) => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round"
+				d="M19.285 12h-8.012m5.237 3.636L20 12l-3.49-3.636M13.455 7V4H4v16h9.455v-3"></path>
+		</svg>)
+}
+
+export default Exit
